5db479358c1849e45d64a02bb5e65fad493f19a1
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-11-16 21:48:00 +0000

    completed player tests

--- lib/game.rb
+++ lib/game.rb
@@ -6,6 +6,7 @@ CHOICES = [:Scissors, :Paper, :Rock, :Lizard, :Spock]
 
 class Player 
   attr_reader :name
+  attr_accessor :choice
   
   def initialize(player_name)
     @name = player_name
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,12 +2,19 @@ require 'game'
 
 describe 'Player' do
 
-  # let(:player) { Player.new("chris") }
+   let(:player) { Player.new("chris") }
   
   it 'should have a name' do
-    player = Player.new('chris')
+    # player = Player.new('chris')
     expect(player.name).to eq('chris')
   end
+
+  it 'should be able to make a choice' do
+    player.choice = :Rock
+    expect(player.choice).to eq(:Rock)
+  end
+
+
 
 
 end
