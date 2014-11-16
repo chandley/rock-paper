9cbbd497216f21337c91d7846111fd0230328d50
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-11-16 22:18:55 +0000

    game logic works RPS

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,7 @@
 
 #beat class which is an odd number of slots after
 CHOICES = [:Scissors, :Paper, :Rock]
-CHOICES = [:Scissors, :Paper, :Rock, :Lizard, :Spock]
+# CHOICES = [:Scissors, :Paper, :Rock, :Lizard, :Spock]
 
 
 class Player 
@@ -16,18 +16,50 @@ class Player
     choice = CHOICES.sample
   end
 
-end
-
-
-
-def game_outcome(player1, player2)
-  case true
-  when player1.choice == player2.choice
-    nil #draw
-  when (CHOICES.index(player1.choice) - CHOICES.index(player2.choice))%2 == 1
-    player1
-  else
-    player2
+  def choices_index
+    CHOICES.index(choice)
   end
+
 end
-    
+
+class Game
+  attr_reader :players
+  def initialize
+    @players = []
+  end
+
+  def add_player(player)
+    @players << player
+  end
+
+  def winner
+    return nil if @players.map(&:choices_index).uniq.count == 1
+    player1 = players.first
+    player2 = players.last
+    choices_rotate_player1 = CHOICES.rotate(player1.choices_index)
+    diff = choices_rotate_player1.index(player2.choice)
+    return player2 if diff % 2 == 0
+    return player1
+  end
+
+    # return @players.first if (players.first.choices_index - players.last.choices_index)%2 == 1
+    # players.last
+
+
+
+end
+
+
+
+
+# def game_outcome(player1, player2)
+#   case true
+#   when player1.choice == player2.choice
+#     nil #draw
+#   when (CHOICES.index(player1.choice) - CHOICES.index(player2.choice))%2 == 1
+#     player1
+#   else
+#     player2
+#   end
+# end
+#     
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -14,7 +14,50 @@ describe 'Player' do
     expect(player.choice).to eq(:Rock)
   end
 
+end
 
+describe 'Game' do
+
+  let(:player1) {double :player, :choice => :Rock}
+  let(:player2) {double :player, :choice => :Scissors}
+
+  it 'should start with no players' do
+    game = Game.new
+    expect(game.players.count).to eq(0)
+  end
+
+  it 'should be able to add two players' do
+    game = Game.new
+    2.times {game.add_player(player1)}
+    expect(game.players.count).to eq(2) 
+  end
+
+  it 'rock should draw with rock' do
+    game = Game.new
+    game.add_player(alice = Player.new('alice'))
+    game.add_player(bob = Player.new('bob'))
+    alice.choice = :Rock
+    bob.choice = :Rock
+    expect(game.winner).to be_nil
+ end
+
+  it 'rock should beat scissors' do
+    game = Game.new
+    game.add_player(alice = Player.new('alice'))
+    game.add_player(bob = Player.new('bob'))
+    alice.choice = :Rock
+    bob.choice = :Scissors
+    expect(game.winner).to eq(alice)
+ end
+
+   it 'scissors should beat paper' do
+    game = Game.new
+    game.add_player(alice = Player.new('alice'))
+    game.add_player(bob = Player.new('bob'))
+    alice.choice = :Paper
+    bob.choice = :Scissors
+    expect(game.winner).to eq(bob)
+ end
 
 
 end
