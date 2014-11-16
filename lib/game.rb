91ec16f47862b268ec1b79f5b1a49e6f591a9577
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-11-16 08:51:10 +0000

    add game logic untested for win,lose,draw

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,27 @@
 
-CHOICES = %i{Rock Paper Scissors}
+# CHOICES = %i{Rock Paper Scissors}
 WINNERS = {:Rock => :Scissors, :Paper => :Rock, :Scissors => :Paper}
 
 def losers 
   WINNERS.invert
 end
+
+def choices
+  WINNERS.keys.uniq
+end
+    
+def random_choice
+  choices.shuffle.first
+end
+
+def game_outcome(player_choice, computer_choice)
+  case true
+  when winners[player_choice] == computer_choice
+    :player_wins
+  when losers[player_choice] == computer_choice
+    :player_loses
+  else
+    :draw
+  end
+end
+    
